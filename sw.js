862244e4eb560dcbf128750e92adf4d7dd88cf2c
codyhughes480-tech <codyhughes480@gmail.com>
2026-09-15 17:03:14 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Daily Goal Sheet - offline app shell
+var CACHE = 'dgs-v1';
+var FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).then(function(){ return self.skipWaiting(); }));
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){ if(k!==CACHE) return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit) return hit;
+      return fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
+        return res;
+      }).catch(function(){ return caches.match('./index.html'); });
+    })
+  );
+});
